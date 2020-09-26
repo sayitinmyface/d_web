9d6b4dac355abf50f12908dec58a258e73330855
aiegoo <eozz21@gmail.com>
2020-09-27 02:47:30 +0900

    add restapi weather.html model url and views.py

--- web_config/settings.py
+++ web_config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tdcgap15xfm6aum5v!#l09r$5v$68f=md17*_)$d@e==bgxylm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '159.65.8.38']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restapi',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
